e4a55ff71dd61af5c2ab72fe1ef9f7daf7896c54
Avishek-kr <ak251997@gmail.com>
2021-09-20 00:19:03 +0530

    useEffect for API

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React , {useEffect ,useState} from 'react'
+
 
 function Homescreen() {
+    
+
+        useEffect(() => {
+           (async function(){
+               try {
+                   const data = (await axios.get('/api/rooms/getallrooms')).data
+                   console.log(data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }) ()
+            }, [])
     return (
         <div>
             <h1>Home screen</h1>
-        </div>
+            <h1>there are {rooms.length}</h1>
+        </div> 
     )
 }
 
