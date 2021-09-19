7d25826aae84370da5f54248139772884b8d0dd0
Avishek-kr <ak251997@gmail.com>
2021-09-19 23:57:36 +0530

    added route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter , Route, Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <BrowserRouter>
+        <Route path="/home" exact component={Homescreen} />
+      </BrowserRouter>
     </div>
   );
 }
