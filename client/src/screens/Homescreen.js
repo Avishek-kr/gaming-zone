709380c6cc0508069efb29944cebf096e07128b0
Avishek-kr <ak251997@gmail.com>
2021-09-20 00:38:29 +0530

    render room names and loading or error

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -3,22 +3,29 @@ import React , {useEffect ,useState} from 'react'
 
 
 function Homescreen() {
-    
+    const [rooms , setrooms] =useState([])
+    const [loading , setloading] =useState()
+    const [error , seterror] =useState()
 
         useEffect(() => {
            (async function(){
                try {
+                setloading(true)
                    const data = (await axios.get('/api/rooms/getallrooms')).data
-                   console.log(data);
+                   setrooms(data);
+                   setloading(false)
                 } catch (error) {
+                    seterror(true)
                     console.log(error);
+                    setloading(false)
                 }
             }) ()
             }, [])
     return (
         <div>
-            <h1>Home screen</h1>
-            <h1>there are {rooms.length}</h1>
+            {loading ? (<h1>loading...</h1>) : error ? (<h1>error</h1>): (rooms.map(room=>{
+                return <h1>{room.name}</h1>
+            }))}
         </div> 
     )
 }
